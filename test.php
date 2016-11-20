6e4ad87f436926d0f5656eeae5a38b15b857319e
Arnold Myint <blinkmonster@gmail.com>
2016-11-19 20:35:44 -0800

    added swift mailer 2

--- test.php
+++ test.php
@@ -3,16 +3,16 @@ require_once ('vendor/autoload.php');
 require_once ('vendor/swiftmailer/swiftmailer/lib/swift_required.php');
 
 
-$transporter = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
+$transporter = \Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
     ->setUsername('smn.vancouver')
     ->getPassword('comp3975SMNam');
 
-$message = Swift_Message::newInstance($transporter);
+$message = \Swift_Message::newInstance($transporter);
 $message -> setTo(array("info@sell-my-notes.com" => "Info SMN"));
 
 $message -> setSubject("This is a test email");
 $message -> setBody("This is the body of that test email");
 $message -> setFrom ("arnold@me.com", "Your friend");
 
-$mailer = Swift_Mailer::newInstance($transporter);
+$mailer = \Swift_Mailer::newInstance($transporter);
 $mailer->send($message);
